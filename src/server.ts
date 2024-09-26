fedef7893fd2473ff709a00ddbaef87fb252b5ec
Leonardo Vales <leohenrique.vales@gmail.com>
2024-09-26 14:29:20 -0300

    inicia testes como vitest

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,12 @@ import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
+/**
+ * Testes unitários: testa uma unidade da aplicação
+ * Testes de integração: Testa a comunicação entre duas ou mais unidades
+ * Testes e2e ou ponta a ponta: Simulam um usuário operando a aplicação
+ */
+
 app.register(cookie)
 app.register(transactionsRoutes, {
   prefix: 'transactions',
---
+++ test/example.spec.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+
+test('Deve testar', () => {
+  const response = 201
+  expect(response).toEqual(201)
+})
